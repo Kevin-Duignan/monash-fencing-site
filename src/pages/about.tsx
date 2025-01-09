bac8177a7ea614cf8c0d70b9cc8a8c8e6a7c8470
Kevin Duignan <kevin.nguyen200476@gmail.com>
2025-01-09 14:51:23 +1100

    added photo plugins to about and index pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, HeadFC } from "gatsby";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>
-        Hi there! I'm the proud creator of this site, which I built with Gatsby.
-      </p>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>I made this bitch with Gatsby. -Kevin the Club Secretary</p>
+      <StaticImage
+        alt="Two female foil fencers in close combat, one is standing high, attempting a parry, while the other is in a lunge, with her blade bent and on target."
+        src="https://upload.wikimedia.org/wikipedia/commons/2/24/Final_2013_Fencing_WCH_FMS-IN_t200907.jpg"
+      />
+    </Layout>
   );
 };
 
-export const Head = () => <title>About Me</title>;
+export const Head: HeadFC = () => <title>About Me</title>;
 
 export default AboutPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Monash Fencers posing, from left to right: Rhys, Daniel (with silver medal), Kevin (with gold medal), Chloe, and Warren (Club coach)"
+        src="/Users/kevinnguyen/Downloads/IMG_1837.jpeg"
+      />
     </Layout>
   );
 };
